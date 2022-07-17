1f4c77a68f1407d4a8dbeeca442e4cedd84c3a6a
soorenaganji <109369184+soorenaganji@users.noreply.github.com>
2022-07-17 22:13:22 +0430

    sending data with a fake API

--- main.js
+++ main.js
@@ -32,7 +32,21 @@ setTimeout(() => {
 } , 3500)
 }
 if (sendingData) {
-    
+    const body = {
+        username : usernamevalue
+     ,  passWord : passwordvalue
+    }
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+  method: 'POST',
+  body: JSON.stringify(body),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+})
+  .then((response) => {
+    if(response.ok){
+document.querySelector(".signin-status").innerHTML = "SUCCESS!"
+    }})
 }
 }
 
